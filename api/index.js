990062c18b88b727e83194ac675e6ce9e2c7c6c6
rodelgabriel45@gmail.com <rodelgabriel45@gmail.com>
2024-05-06 15:59:06 +0800

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,9 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO)
@@ -17,3 +21,17 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
