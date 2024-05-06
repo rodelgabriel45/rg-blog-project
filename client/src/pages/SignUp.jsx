cc7bcf748094c3812ed18fdb85826b2a3ee9ab6b
rodelgabriel45@gmail.com <rodelgabriel45@gmail.com>
2024-05-06 18:10:45 +0800

    add signup page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,73 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUpPage() {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [userData, setUserData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        setError(false);
+      }, 4000);
+    }
+
+    if (success) {
+      setTimeout(() => {
+        setSuccess(false);
+        navigate("/signin");
+      }, 4000);
+    }
+
+    return () => {
+      clearTimeout();
+    };
+  }, [error, success]);
+
+  const handleChange = (e) => {
+    setUserData({
+      ...userData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const response = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+
+      const resData = await response.json();
+
+      if (resData.success === false) {
+        setLoading(false);
+        setError(resData);
+        return;
+      }
+
+      setLoading(false);
+      setSuccess(resData);
+    } catch (error) {
+      setLoading(false);
+      setError(error);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex flex-col md:flex-row md:items-center p-3 max-w-3xl mx-auto gap-5">
@@ -20,23 +86,59 @@ export default function SignUpPage() {
         </div>
 
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
             <div>
               <Label value="Username" />
-              <TextInput type="text" placeholder="Enter your username..." />
+              <TextInput
+                type="text"
+                id="username"
+                onChange={handleChange}
+                required
+              />
             </div>
             <div>
               <Label value="Email" />
-              <TextInput type="text" placeholder="name@company.com" />
+              <TextInput
+                type="email"
+                id="email"
+                placeholder="name@company.com"
+                onChange={handleChange}
+                required
+              />
             </div>
             <div>
               <Label value="Password" />
-              <TextInput type="text" placeholder="Enter your password..." />
+              <TextInput
+                type="password"
+                id="password"
+                onChange={handleChange}
+                required
+              />
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">
-              Sign Up
+            <Button
+              disabled={loading}
+              gradientDuoTone="purpleToPink"
+              type="submit"
+            >
+              {loading ? (
+                <div className="flex gap-2">
+                  <Spinner size="sm" /> <span>Signing Up...</span>
+                </div>
+              ) : (
+                "Sign Up"
+              )}
             </Button>
           </form>
+          {error && (
+            <p className="text-red-500 font-semibold mt-3">
+              {error.message || error}
+            </p>
+          )}
+          {success && (
+            <p className="text-green-500 font-semibold mt-3">
+              {success.message} redirecting to sign in... <Spinner size="sm" />
+            </p>
+          )}
           <div className="mt-5 flex gap-2">
             <span>Have an account?</span>
             <Link to="/signin" className="text-blue-500 hover:opacity-70">
