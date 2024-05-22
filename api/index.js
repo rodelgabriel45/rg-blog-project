0a8413d9e9fc3bae3f673dc931a75f5b76003b85
rodelgabriel45@gmail.com <rodelgabriel45@gmail.com>
2024-05-23 01:46:13 +0800

    complete app / add pre installation

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
@@ -26,10 +27,19 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
+// API Routes
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+
+// static pages for frontend
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
